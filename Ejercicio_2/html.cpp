029781405dc1608001a5c202e084fc8fe41feb80
unknown <yrlen.perez@unet.edu.ve>
2017-03-18 19:30:39 -0400

    Tags de cierre..

--- Ejercicio_2/html.cpp
+++ Ejercicio_2/html.cpp
@@ -10,6 +10,7 @@ using namespace std;
 int main(int argc, char** argv) {
 	string buffer,copia;
 	int tam;
+	vector< int > almacen;
 	Archivo *archi = new Archivo( "html.txt" );
 	archi->abrir();
 	while( !archi->isFinal() )
@@ -26,6 +27,17 @@ int main(int argc, char** argv) {
 					if(isupper(h[0]))
 					{
 						cout<<h<<endl;	
+					}
+					
+				}
+				else if( buffer[ i ] == '<' && buffer[ i+3 ] == '>' )
+				{
+					char* h = new char[ buffer.length() +1 ];
+					copia=buffer[i+2];
+					strcpy(h,copia.c_str());
+					if(isupper(h[0]) && buffer[ i+1 ] == '/' )
+					{
+						cout<<"/"<<h<<endl;	
 					}
 					
 				}
