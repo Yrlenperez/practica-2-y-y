69c2ccfcc5360877412fd533bf031bb5d2ec9e72
unknown <yrlen.perez@unet.edu.ve>
2017-03-19 12:44:43 -0400

    convension y bugs

--- Ejercicio_2/html.cpp
+++ Ejercicio_2/html.cpp
@@ -10,15 +10,15 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	string buffer, copia;
-	int tam, tama, tama2, band=0, band1=0, cont=1, con =0, div;
+	int tam, tama, tama2, band = 0, band1 = 0, cont = 1, con = 0, div;
 	vector< char > almacen;
 	Archivo *archi = new Archivo( "html.txt" );
-	archi->abrir();
-	while( !archi->isFinal() )
+	archi->abrir( );
+	while( !archi->isFinal( ) )
 		{
 			buffer = archi->leerCampo( '\n' );
 			tam = strlen( buffer.c_str() );
-			band = band1 = 0;
+			band = band1 = con = div = tama = 0;
 			//cout<<endl<<buffer<<endl;
 			for( int i = 0; i < tam; i++ )
 			{
@@ -48,60 +48,65 @@ int main(int argc, char** argv) {
 			if( buffer[ tam - 1 ] == '#' )
 			{
 				tama = almacen.size();
-				div=tama/2;
+				div = tama / 2;
 				if(tama % 2 == 0)
 				{
 					if( almacen[0] == almacen[tama-1] )
 					{
-						for(int i=tama/2,j=i-1; i>0 && 
-						i<tama-1 && j>0 && j<tama-1;i++,j--)
+						for( int i = tama / 2, j = i - 1; i > 0 && 
+						i < tama - 1 && j > 0 && j < tama - 1; i++, j-- )
 						{
-							if( almacen[i]== almacen[j] )
+							if( almacen[ i ] == almacen[ j ] )
 							{
-								if(band==0)
+								if( band == 0 )
 								{
-									cout<<"Caso #"<<cont
-									<<": Codigo valido"<<endl;
-									band=1;
+									cout << "Caso #" << cont
+									<< ": Codigo valido" << endl;
+									band = 1;
 								}	
 							}		
 						}	
 					}
 					else 
 						{
-							for(int i=tama/2,j=i-1; i>0 && 
-							i<tama-1 && j>0 && j<tama-1;i++,j--)
+							for( int i = tama / 2, j = i - 1; i > 0 && 
+							i < tama - 1 && j > 0 && j < tama - 1; i++, j--)
 							{
-								if( almacen[i] != almacen[j] )
+								if( almacen[ i ] != almacen[ j ] )
 								{
 									if( band1 == 0 )
 									{
-										cout<<"caso #"<<cont<<": Se esperaba </"<<almacen[j]
-										<<"> pero se encontro </"<<almacen[i]<<">"<<endl;
-										band1=1;
+										cout << "caso #" << cont << ": Se esperaba </" 
+										<< almacen[ j ]	<< "> pero se encontro </" 
+										<< almacen[ i ] << ">" << endl;
+										band1 = 1;
 									}
 								}
 							}	
-						}	
+						}
 				}
-				else if( almacen[0] != almacen[tama-1])
+				else if( almacen[ 0 ] != almacen[ tama-1 ])
 				{ 
-				while(con<almacen.size() && con+1<almacen.size()){
-					if(almacen[con]==almacen[con+1]){
-						almacen.erase(almacen.begin()+con,almacen.begin()+con+2);
-						if(con>=div)	{
-					cout<<"Caso #"<<cont<<": Se esperaba </"
-					<<almacen[0]<<"> pero se encontro #"<<endl;	
+					while( con < almacen.size() && con + 1 < almacen.size() )
+					{
+						if( almacen[ con ] == almacen[ con + 1 ] )
+						{
+							almacen.erase( almacen.begin() + con,
+							almacen.begin() + con + 2 );
+							if( con >= div)
+							{
+								cout << "Caso #" << cont << ": Se esperaba </"
+								<< almacen[ 0 ] << "> pero se encontro #" << endl;	
+							}
+							else if( con <= div )
+							{
+								cout << "Caso #" << cont << ": Se esperaba #"
+								<< " pero se encontro </" << almacen[ 0 ] << ">" << endl;		
+							}
+						}
+						con++;
 					}
-					else if(con<=div){
-					cout<<"Caso #"<<cont<<": Se esperaba #"
-					<<" pero se encontro </"<<almacen[0]<<">"<<endl;		
-					}
-					}
-					con++;
 				}
-				}
-		
 				almacen.clear();
 			}
 			cont++;	
